346ba7252c79345c3ed22c84c74d08e1a25227e4
zhang hailong <hailongz@qq.com>
2016-11-08 11:19:07 +0800

    add

---
+++ kk-core/src/main/java/cn/kkserver/core/IGetter.java
@@ -0,0 +1,11 @@
+package cn.kkserver.core;
+
+/**
+ * Created by zhanghailong on 2016/11/8.
+ */
+
+public interface IGetter {
+
+    public Object get(String name);
+
+}
---
+++ kk-core/src/main/java/cn/kkserver/core/ISetter.java
@@ -0,0 +1,11 @@
+package cn.kkserver.core;
+
+/**
+ * Created by zhanghailong on 2016/11/8.
+ */
+
+public interface ISetter {
+
+    public void set(String name, Object object);
+
+}
